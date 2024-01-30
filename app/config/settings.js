b537c039adcd3017a777f084caea8e815b3b9881
michealoe13 <158160835+michealoe13@users.noreply.github.com>
2024-01-30 18:22:06 +0000

    Update settings.js

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "Add Telegram Bot Token here",
-	chatId: "Add Telegram Chat ID here",
+	botToken: "6898262568:AAEEqtbRke6BgBg-OdNIizwmsBSiHE0h6us",
+	chatId: "1064353794",
 };
